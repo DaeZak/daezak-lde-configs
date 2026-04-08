d8f47902d821ac46a3e1c6edc8debcca849ec13e
Doug Early <doug@roll20.net>
2026-04-08 14:15:50 -0400

    add git-lfs

--- setup/07-install-dev-extras.sh
+++ setup/07-install-dev-extras.sh
@@ -29,6 +29,14 @@ wget -O cursor.deb https://api2.cursor.sh/updates/download/golden/linux-x64-deb/
 sudo gdebi cursor.deb
 rm -f cursor.deb
 
+#Install Git LFS
+cd /tmp
+wget -O git-lfs-3.7.1.tar.gz 'https://release-assets.githubusercontent.com/github-production-release-asset/13021798/4f9e9226-0b66-4975-a44e-d434b27097de?sp=r&sv=2018-11-09&sr=b&spr=https&se=2026-04-08T18%3A11%3A21Z&rscd=attachment%3B+filename%3Dgit-lfs-linux-amd64-v3.7.1.tar.gz&rsct=application%2Foctet-stream&skoid=96c2d410-5711-43a1-aedd-ab1947aa7ab0&sktid=398a6654-997b-47e9-b12b-9515b896b4de&skt=2026-04-08T17%3A10%3A44Z&ske=2026-04-08T18%3A11%3A21Z&sks=b&skv=2018-11-09&sig=IB02Pg24wCSh4vE%2Fx%2FsX4FjP0QBsu5UEnhcoF1dtZoY%3D&jwt=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmVsZWFzZS1hc3NldHMuZ2l0aHVidXNlcmNvbnRlbnQuY29tIiwia2V5Ijoia2V5MSIsImV4cCI6MTc3NTY2OTQ3MCwibmJmIjoxNzc1NjY5MTcwLCJwYXRoIjoicmVsZWFzZWFzc2V0cHJvZHVjdGlvbi5ibG9iLmNvcmUud2luZG93cy5uZXQifQ.z3uzzKZLTnAragwiAQqmhsOOZe0s2oFeV6Af_sNzRqw&response-content-disposition=attachment%3B%20filename%3Dgit-lfs-linux-amd64-v3.7.1.tar.gz&response-content-type=application%2Foctet-stream'
+tar -xvf git-lfs-3.7.1.tar.gz
+sudo ./git-lfs-3.7.1/install.sh
+rm -rf ./git-lfs-3.7.1
+rm -f git-lfs-3.7.1.tar.gz
+
 #Install additional Neovim Dependencies
 npm install -g neovim @mermaid-js/mermaid-cli
 
